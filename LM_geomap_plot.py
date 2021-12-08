67835ac8d0c482aee441d543a1640636ed17e0e0
Luis Barrancos <luis.b.barrancos@gmail.com>
2021-12-08 23:17:46 +0000

    Cleanups

--- LM_geomap_plot.py
+++ LM_geomap_plot.py
@@ -5,16 +5,14 @@ Created on Sat Dec  4 22:17:25 2021
 
 @author: cgwork
 """
-import logging
 import os
 
-import dash
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 from dash import dcc, html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 
 from app import app
@@ -33,8 +31,8 @@ datapath = os.path.join(os.getcwd(), "resources", "dbs")
 df = pd.read_sql_table(
     "Deadline_database",
     "sqlite:///" + os.path.join(datapath, "deadline_database_nonans_geo.db"),
-    index_col = "Country"
-    )
+    index_col="Country",
+)
 
 # df.dropna(inplace=True)
 df.sort_values(by=["Year"], inplace=True)
@@ -46,19 +44,6 @@ df = df[df["Year"] >= 2000]
 countries = list(df.index.unique())
 country_options = [{"label": str(val), "value": str(val)} for val in countries]
 
-
-# Dash
-# =============================================================================
-# external_stylesheets = [dbc.themes.DARKLY]
-#
-# app = dash.Dash(
-#     __name__,
-#     external_stylesheets=external_stylesheets,
-#     assets_url_path=os.path.join(os.getcwd(), "assets"),
-# )
-# app.title = "Deadline"
-#
-# =============================================================================
 
 # Year/range slider
 year_min = df["Year"].min()
@@ -299,27 +284,20 @@ def color_countries_and_region(country, years, datafield, n_clicks):
     line_fig.update_layout(
         title=str(datafield).replace("_", " ").title() + " (World Map)"
     )
+    # https://plotly.com/python/colorscales/
+    line_fig.update_layout(
+        coloraxis_colorbar=dict(
+            title=datafield.replace("_", " ").title(),
+            titleside="right",
+            thicknessmode="pixels",
+            thickness=20,
+            # lenmode="pixels", len=200,
+            # yanchor="top", y=1,
+            # xanchor="right", x=0.3,
+            # ticks="inside",
+            # ticksuffix=" bills",
+            # dtick=5
+        )
+    )
+
     return line_fig
-
-
-# =============================================================================
-#
-# if __name__ == "__main__":
-#     # app.run_server(debug=True)
-#     app.run_server(
-#         host="127.0.0.1",
-#         port="8050",
-#         proxy=None,
-#         debug=True,
-#         # dev_tools_props_check=None,
-#         # dev_tools_serve_dev_bundles=None,
-#         # dev_tools_hot_reload=None,
-#         # dev_tools_hot_reload_interval=None,
-#         # dev_tools_hot_reload_watch_interval=None,
-#         # dev_tools_hot_reload_max_retry=None,
-#         # dev_tools_silence_routes_logging=None,
-#         # dev_tools_prune_errors=None,
-#         # **flask_run_options
-#     )
-#
-# =============================================================================
